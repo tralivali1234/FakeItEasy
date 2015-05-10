5f0e97c0fc70cde06edf3e67484df1fc47806082
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2015-05-10 04:40:26 +0200

    Add analyzer to warn about incorrect usage
    There's only one analyzer so far; it checks for unused call
    specifications, i.e. calls to A.CallTo(...) that are not used.
    At this point the analysis doesn't try to be very smart, it just check if
    the call to A.CallTo(...) is the only thing in the statement.

---
+++ Source/FakeItEasy.Analyzers/FakeItEasy.Analyzers/SymbolExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+
+namespace FakeItEasy.Analyzers
+{
+    public static class SymbolExtensions
+    {
+        public static string GetFullName(this INamedTypeSymbol type)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            var stack = new Stack<string>();
+            stack.Push(type.Name);
+            var tmp = type.ContainingType;
+            while (tmp != null)
+            {
+                type = tmp;
+                stack.Push(tmp.Name);
+                tmp = tmp.ContainingType;
+            }
+            
+            var ns = type.ContainingNamespace;
+            while (!string.IsNullOrEmpty(ns?.Name))
+            {
+                stack.Push(ns.Name);
+                ns = ns.ContainingNamespace;
+            }
+            return string.Join(".", stack);
+        }
+    }
+}
